4fcdc3cf532554db63df1aefc4f2c709399ce7d6
Justin Pauga <paugajt@gmail.com>
2022-07-21 21:21:48 -0700

    Initial Commit

---
+++ Shared/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  Shared
+//
+//  Created by Justin Pauga on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
